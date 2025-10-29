0af0b6c6ee69941de15d294b7411e81df87ba22a
QUORIX\quori <kaiserdoragon1121@gmail.com>
2025-10-29 15:27:56 +0900

    モーダルウィンドウのコードを修正・デバッグ

--- theme/js/scripts.js
+++ theme/js/scripts.js
@@ -184,4 +184,33 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 
+
+
+  // 変数に要素を入れる
+  var trigger = $('.js-modal_trigger'),
+    wrapper = $('.modal__wrapper'),
+    layer = $('.modal__layer'),
+    container = $('.modal__container'),
+    close = $('.modal__close');
+
+  // 『モーダルを開くボタン』をクリックしたら、『モーダル本体』を表示
+  $(trigger).click(function () {
+    var index = $(this).index();
+    $(wrapper).eq(index).fadeIn(400);
+
+    // スクロール位置を戻す
+    $(container).scrollTop(0);
+
+    // サイトのスクロールを禁止にする
+    $('html, body').css('overflow', 'hidden');
+  });
+
+  // 『背景』と『モーダルを閉じるボタン』をクリックしたら、『モーダル本体』を非表示
+  $(layer).add(close).click(function () {
+    $(wrapper).fadeOut(400);
+
+    // サイトのスクロール禁止を解除する
+    $('html, body').removeAttr('style');
+  });
+
 })(jQuery, this);
